2b18f4518c11259d93e78513193743d89c961827
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-11-15 23:06:16 -0500

    Cambiamos logica del appRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,17 +1,13 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { MarvelPage, DcPage } from '../heroes';
+import { Routes, Route } from 'react-router-dom';
+import { HeroesRoutes } from '../heroes';
 import { LoginPage } from '../auth';
-import { Navbar } from '../ui';
 
 export const AppRouter = () => {
     return (
         <>
-            <Navbar />
             <Routes>
-                <Route path="/marvel" element={<MarvelPage />} />
-                <Route path="/dc" element={<DcPage />} />
                 <Route path="/login" element={<LoginPage />} />
-                <Route path="/" element={<Navigate to="/login" />} />
+                <Route path="/*" element={<HeroesRoutes />} />
             </Routes>
 
         </>
