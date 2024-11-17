87968ddd45e97f12559d524c1b8494ea13a2162d
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-11-16 22:28:11 -0500

    Tarjeta del Héroe

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,22 +1,37 @@
+import { Link } from "react-router-dom";
+
+const CharactersByHero = ({ alter_ego, characters }) => {
+    return (alter_ego === characters)
+        ? <></>
+        : <p className="card-text">{characters}</p>
+}
+
 export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters }) => {
+
+    const heroImgUrl = `../assets/heroes/${id}.jpg`;
+    // const charactersByHero = (<p className="card-text">{characters}</p>)
+
+
     return (
 
         <div className="col animate__animated animate__fadeIn">
             <div className="card">
                 <div className="row no-gutters">
                     <div className="col-4">
-                        <img src={`../assets/heroes/${id}.jpg`} className="card-img" alt={superhero} />
+                        <img src={heroImgUrl} className="card-img" alt={superhero} />
                     </div>
                     <div className="col-8">
                         <div className="card-body">
                             <h5 className="card-title">{superhero}</h5>
                             <p className="card-text">{alter_ego}</p>
-                            {
-                                (alter_ego !== characters)
-                                && <p className="card-text">{characters}</p>
-                            }
-                            <p className="card-text"><small className="text-muted">{first_appearance}</small></p>
+
+                            <CharactersByHero alter_ego={alter_ego} characters={characters} />
+
+                            <p className="card-text">
+                                <small className="text-muted">{first_appearance}</small>
+                            </p>
                             <p className="card-text">{publisher}</p>
+                            <Link to={`/hero/${id}`}>Más...</Link>
                         </div>
                     </div>
                 </div>
