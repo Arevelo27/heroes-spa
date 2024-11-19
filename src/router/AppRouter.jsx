d7c59a0cf6d12d754fd504f95ed65a57c2bf748c
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-11-18 21:04:13 -0500

    Rutas públicas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,12 +2,20 @@ import { Routes, Route } from 'react-router-dom';
 import { PrivateRoute } from './PrivateRoute';
 import { HeroesRoutes } from '../heroes';
 import { LoginPage } from '../auth';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
     return (
         <>
             <Routes>
-                <Route path="/login" element={<LoginPage />} />
+                <Route path="/login" element={
+                    <PublicRoute>
+                        {/* <LoginPage /> */}
+                        <Routes>
+                            <Route path="/*" element={<LoginPage />} />
+                        </Routes>
+                    </PublicRoute>
+                } />
 
                 <Route path="/*" element={
                     <PrivateRoute>
