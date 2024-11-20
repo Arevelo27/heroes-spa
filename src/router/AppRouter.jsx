e41c18581689eb93954d822b2f0cf58362119a3c
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-11-19 22:35:40 -0500

    Pruebas en el AppRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
-import { PrivateRoute } from './PrivateRoute';
 import { HeroesRoutes } from '../heroes';
 import { LoginPage } from '../auth';
+import { PrivateRoute } from './PrivateRoute';
 import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
     return (
         <>
             <Routes>
-                <Route path="/login" element={
+                <Route path="login/*" element={
                     <PublicRoute>
                         {/* <LoginPage /> */}
                         <Routes>
