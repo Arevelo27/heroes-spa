52f8172cdcf9b691af9ca40363d9c2b1f370e8bb
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-11-20 00:00:43 -0500

    requireActual

--- test/heroes/pages/SearchPage.test.jsx
+++ test/heroes/pages/SearchPage.test.jsx
@@ -1,12 +1,25 @@
-import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import { SearchPage } from "../../../src/heroes/pages/SearchPage";
 
+const mockedUsedNavigate = jest.fn();
+
 jest.mock('query-string', () => ({
-    parse: jest.fn(() => ({ q: 'batman' })),
+    parse: jest.fn((search) => {
+        const params = new URLSearchParams(search);
+        return { q: params.get('q') || '' };
+    }),
+}));
+
+// Mock de useNavigate si es necesario
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedUsedNavigate,
 }));
 
 describe('Pruebas en <SearchPage />', () => {
+
+    beforeEach(() => jest.clearAllMocks());
 
     test('debe de mostrarse correctamente con valores por defecto', () => {
 
@@ -37,4 +50,38 @@ describe('Pruebas en <SearchPage />', () => {
         expect(alert.style.display).toBe('none');
 
     });
-})  
+
+    test('debe de mostrar un error si no se encuentra el hero (batman123)', () => {
+
+        render(
+            <MemoryRouter initialEntries={['/search?q=batman123']} future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+                <SearchPage />
+            </MemoryRouter>
+        );
+
+        const alert = screen.getByLabelText('alert-danger');
+        expect(alert.style.display).toBe('');
+
+    });
+
+    test('debe de llamar el navigate a la pantalla nueva', () => {
+
+        const inputValue = 'superman';
+
+        render(
+            <MemoryRouter initialEntries={['/search']} future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+                <SearchPage />
+            </MemoryRouter>
+        );
+
+        const input = screen.getByRole('textbox');
+        fireEvent.change(input, { target: { name: 'searchText', value: inputValue } });
+
+        const form = screen.getByRole('form');
+        fireEvent.submit(form);
+
+        expect(mockedUsedNavigate).toHaveBeenCalledWith(`?q=${inputValue}`);
+
+    });
+
+});
